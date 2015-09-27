b300580d3db77f0290036110c6cb41d45d676ef6
Jens <jens@jens>
2015-09-27 16:06:02 +0200

    Use experimental object observers for mocking. only works in chromium for now.

--- killnrcqpaywall.user.js
+++ killnrcqpaywall.user.js
@@ -1,23 +1,37 @@
 // ==UserScript==
 // @name       Kill nrcQ restrictions
 // @namespace  http://use.i.E.your.homepage/
-// @version    0.1
+// @version    0.2
 // @description  When browsing nrcq with more than 10 views you need to sign in to view the content, this plugin disables the lay-over.
 // @match      http://www.nrcq.nl/*
 // @copyright  2015+, You
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// @run-at document-start
+// @grant       none
 // ==/UserScript==
 
-// Load the jquery library.
-this.$ = this.jQuery = jQuery.noConflict(true);
+// initialize the objects
+window.nmt = window.nmt || {};
+nmt.q = nmt.q || {};
 
-$(document).ready(function()
-  {
-    // change the class from opened to closed
-    $(document.body).removeClass("boxy-opened").addClass("boxy-closed");
+// The observer function.
+var observer = function(changes) {
+  changes.forEach(function(change) {
+    // Any time boxy is added to nmt.q
+    if (change.name == 'boxy') {
+      // mock the boxy inDom func.
+      nmt.q.boxy = function(x) {
+        this.inDom = false;
+      }
+      // destroy the observer
+      doUnobserve(nmt.q);
+    };
+  });
+};
 
-    // Remove the overlays.
-    $('div.boxy-overlay').remove();
-    $('div.boxy').remove();
-  }
-);
+// Use experimental feature.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/observe#Browser_compatibility
+Object.observe(nmt.q, observer, ["add"]);
+
+function doUnobserve(obj) {
+  Object.unobserve(obj, observer);
+}
